5599b09e6f94b76388e677c087fbd482f3bf7975
Mahmoud AlBoraey <mahmoudalsayied@gmail.com>
2022-01-31 13:37:24 +0200

    changed images to be png insted of jpg

--- src/assets/homePhotos/skills-images/img-src.js
+++ src/assets/homePhotos/skills-images/img-src.js
@@ -1,17 +1,17 @@
 const image = {
-    writing: require('./writing.jpg').default,
-    salse: require('./salse.jpg').default,
-    voiceOver: require('./voice-over.jpg').default,
-    translators: require('./translators.jpg').default,
-    mediaProduction: require('./media-production.jpg').default,
-    hr: require('./hr.jpg').default,
-    graphicDesign: require('./graphic-designers.jpg').default,
-    finance: require('./finance.jpg').default,
-    digitalMarketing: require('./digital-marketing.jpg').default,
-    developers: require('./developers.jpg').default,
-    consulting: require('./consulting.jpg').default,
-    arctechure: require('./arctechuer.jpg').default,
-    adminstration: require('./adminstration.jpg').default
+    writing: require('./writing.png').default,
+    salse: require('./salse.png').default,
+    voiceOver: require('./voice-over.png').default,
+    translators: require('./translators.png').default,
+    mediaProduction: require('./media-production.png').default,
+    hr: require('./hr.png').default,
+    graphicDesign: require('./graphic-designers.png').default,
+    finance: require('./finance.png').default,
+    digitalMarketing: require('./digital-marketing.png').default,
+    developers: require('./developers.png').default,
+    consulting: require('./consulting.png').default,
+    arctechure: require('./arctechuer.png').default,
+    adminstration: require('./adminstration.png').default
 };
 
 export default image;
--- src/pages/jobs/index.jsx
+++ src/pages/jobs/index.jsx
@@ -7,6 +7,7 @@ const Jobs = () => {
         <div className="jobs-page">
             <section className="jobs-page-intro">
                 <div className="container">
+                    <h1>browse</h1>
                     <form action="">
                         <input type="search" name="job-search" id="" />
                         <input type="button" value="Search" />
@@ -15,7 +16,11 @@ const Jobs = () => {
             </section>
             <div className="container">
 
-                <aside>aside</aside>
+                <aside>
+                    <h2>Filters</h2>
+                    <h3>Project type</h3>
+
+                </aside>
                 <main>main</main>
             </div>
         </div>
