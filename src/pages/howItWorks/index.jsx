7c47a019220f26132549fda856cfff004ef3ef79
Mahmoud AlBoraey <mahmoudalsayied@gmail.com>
2022-02-08 21:10:58 +0200

    done how it workes page just need to change the font size to mobile view

--- src/pages/howItWorks/index.jsx
+++ src/pages/howItWorks/index.jsx
@@ -1,12 +1,176 @@
 import React from "react";
-import './../../global/global-style.scss';
+import { BsFillQuestionCircleFill } from 'react-icons/bs'
 import './style.scss';
 
 const HowItWorks = () => {
     return (
-        <div>
-            <h1>HowItWorks</h1>
-        </div>
+        <main>
+            <section className="page-intro">
+                <div className="content">
+                    <div className="container">
+                        <div className="help-your-business">
+                            <h1><BsFillQuestionCircleFill /> How it works?</h1>
+                            <p className="sub-title">Easy &amp; Fast
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="hire-freelancer">
+                <div className="hire-freelancer-container">
+                    <div className="hire-freelancer-content">
+                        <h2 className="hire-freelancer-title">Looking for freelancer?</h2>
+                        <div className="hire-freelancer-cards-section">
+                            <h3 className="hire-freelancer-cards-section-title">1- Find Your Perfect Match</h3>
+                            <div className="hire-freelancer-cards">
+                                <div className="card">
+                                    <h4 className="card-title">Create account</h4>
+                                    <p className="card-content">1- Create a free account and post your first job with as many details as possible so you can find the right person for your project.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <h4 className="card-title">Get proposals</h4>
+                                    <p className="card-content">2- Once you post a job, you’ll get proposals from qualified freelancers whose profiles you can browse.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <h4 className="card-title">Candidates</h4>
+                                    <p className="card-content">3- Create a shortlist of potential candidates and chat with them about the scope of your project until you decide on your perfect match.
+
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="hire-freelancer-cards-section">
+                            <h3 className="hire-freelancer-cards-section-title">2- Let’s Get To Work!</h3>
+                            <div className="hire-freelancer-cards">
+                                <div className="card">
+                                    <h4 className="card-title">Your payment</h4>
+                                    <p className="card-content">1- Once you and your freelancer are ready to get things started, make your payment through us and we’ll hold your money until the project is done and you decide it’s ready to be released to the freelancer. Don’t worry; no money will be released without your approval.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <h4 className="card-title">files, documents</h4>
+                                    <p className="card-content">2- Share all your files and documents with your freelancer through chat so you can keep everything in one place.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <h4 className="card-title">Done</h4>
+                                    <p className="card-content">3- Once your freelancer is done with the project, we’ll notify you!
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="hire-freelancer-cards-section">
+                            <h3 className="hire-freelancer-cards-section-title">3- Money Talks</h3>
+                            <div className="hire-freelancer-cards">
+                                <div className="card">
+                                    <h4 className="card-title">Fees</h4>
+                                    <p className="card-content">1- Once you go through the deliverables and make sure you got exactly what you wanted, rate your freelancer and leave a comment about your experience – then we’ll release the payment after deducting our gate fees (20%) from the total budget from the freelancer’s side.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <h4 className="card-title">Payment Methods</h4>
+                                    <p className="card-content">2- There are plenty of ways you can pay for your project: credit, debit, cash on delivery.
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </section>
+            <div className="hr-container">
+                <hr />
+            </div>
+
+            <section className="post-job">
+                <div className="job-advisor-container">
+                    <div className="job-advisor-content">
+                        <h2 className="job-advisor-title">So what are you waiting for?</h2>
+                        <h3 className="job-advisor-sup-title">Post a project today and get bids from talented freelancers.</h3>
+                        <button className="job-advisor-sup-button">Post a Project</button>
+                    </div>
+                </div>
+
+            </section>
+
+            <div className="hr-container">
+                <hr />
+            </div>
+
+            <section className="earn-money-freelancing">
+                <div className="earn-money-container">
+                    <div className="earn-money-content">
+                        <h2 className="earn-money-title">Want to earn money freelancing?</h2>
+                        <div className="earn-money-cards-section">
+                            <h3 className="earn-money-cards-section-title">1- Start Your Hustle</h3>
+                            <div className="earn-money-cards">
+                                <div className="card">
+                                    <h4 className="card-title">Create account</h4>
+                                    <p className="card-content">1- Create a free account and fill out your profile so we can get you verified and active on the website.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <h4 className="card-title">Browse jobs</h4>
+                                    <p className="card-content">2- Browse through the available jobs in your field and pick the ones you’re interested in.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <h4 className="card-title">Find job</h4>
+                                    <p className="card-content">3-  Once you find a job that works for you, draft your proposal and hit send!
+                                    </p>
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div className="earn-money-cards-section">
+                            <h3 className="earn-money-cards-section-title">1- Let’s Get To Work!</h3>
+                            <div className="earn-money-cards">
+                                <div className="card">
+                                    <h4 className="card-title">Get selected</h4>
+                                    <p className="card-content">1- Once the clients selects you as their perfect match, you’re ready to get started.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <h4 className="card-title">files, documents</h4>
+                                    <p className="card-content">2- Share all your files and documents with your client through chat so you can keep everything in one place.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <h4 className="card-title">deliver</h4>
+                                    <p className="card-content">3- Make sure you send all your deliverables on time, then hit ‘end the project’ to let your client know that you’re all done.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <h4 className="card-title">Rate</h4>
+                                    <p className="card-content">3- How was your experience? Rate your client and leave your comments about it.
+                                    </p>
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div className="earn-money-cards-section">
+                            <h3 className="earn-money-cards-section-title">3- Money Talks</h3>
+                            <div className="earn-money-cards">
+                                <div className="card">
+                                    <h4 className="card-title">Get Money</h4>
+                                    <p className="card-content">1- Once your client says the magic words, we’ll send your money after we take our fees 10% from the total budget, that is..
+                                    </p>
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 }
 
