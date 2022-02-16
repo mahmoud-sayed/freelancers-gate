1c35a8f05f1acaf5a00efae2a1940f376f9d27df
Mahmoud AlBoraey <mahmoudalsayied@gmail.com>
2022-02-16 17:33:32 +0200

    done proposals page for laptop veiw nex will work on tablet and mobile view and will work on job description page

--- src/components/proposals/index.jsx
+++ src/components/proposals/index.jsx
@@ -1,15 +1,192 @@
 import React from "react";
 import './style.scss';
+import RatingStars from './../ratingStars';
 const Proposals = () => {
   return (
     <section className="proposals-page-wrapper">
       <section className="proposals-wrapper">
-        <div className="proposal"></div>
-        <div className="proposal"></div>
-        <div className="proposal"></div>
+        <div className="proposal">
+          <div className="proposal-header">
+            <div className="proposal-header-img-freelancerInfo">
+              <div className="proposal-header-image">
+                <img src="https://via.placeholder.com/80x64" alt="freelancer photo" />
+              </div>
+              <div className="proposal-header-description">
+
+                <div className="name-tag">
+                  <div className="flag">
+                    <img src="https://via.placeholder.com/21x16" alt="flag of country name" />
+                  </div>
+                  <h2 className="freelancer">Mahmoud S.</h2>
+                  <p className="freelancer-name-tag">@mamoudBoraye88</p>
+                </div>
+                <div className="freelancer-rating">
+                  <RatingStars />
+                </div>
+
+
+              </div>
+            </div>
+            <div className="proposal-header-rate-time">
+              <h2 className="proposal-rate">$500.00 USD</h2>
+              <p className="proposal-estimation">in 7 days</p>
+            </div>
+          </div>
+          <div className="proposal-body">
+            <p>Hello, Hireprofl.
+              Reading job description carefully and noticed that you are going to develop job board with react and node js.
+              As MERN stack developer I am confident to complete this project and I can charge above requires.
+              I hope to work for you and I am sure that I will give you perfect result.
+              https://darkgalaxies.io/
+              https://www.gfinityesports.com/halo-infinite/halo-circle-store/
+              Thanks.
+              Best Regards.
+              Evegenia.</p>
+          </div>
+          <div className="proposal-footer">
+            <p>Report applied</p>
+          </div>
+        </div>
+        <div className="proposal">
+          <div className="proposal-header">
+            <div className="proposal-header-img-freelancerInfo">
+              <div className="proposal-header-image">
+                <img src="https://via.placeholder.com/80x64" alt="freelancer photo" />
+              </div>
+              <div className="proposal-header-description">
+
+                <div className="name-tag">
+                  <div className="flag">
+                    <img src="https://via.placeholder.com/21x16" alt="flag of country name" />
+                  </div>
+                  <h2 className="freelancer">Mahmoud S.</h2>
+                  <p className="freelancer-name-tag">@mamoudBoraye88</p>
+                </div>
+                <div className="freelancer-rating">
+                  <RatingStars />
+                </div>
+
+
+              </div>
+            </div>
+            <div className="proposal-header-rate-time">
+              <h2 className="proposal-rate">$500.00 USD</h2>
+              <p className="proposal-estimation">in 7 days</p>
+            </div>
+          </div>
+          <div className="proposal-body">
+            <p>Hello, Hireprofl.
+              Reading job description carefully and noticed that you are going to develop job board with react and node js.
+              As MERN stack developer I am confident to complete this project and I can charge above requires.
+              I hope to work for you and I am sure that I will give you perfect result.
+              https://darkgalaxies.io/
+              https://www.gfinityesports.com/halo-infinite/halo-circle-store/
+              Thanks.
+              Best Regards.
+              Evegenia.</p>
+          </div>
+          <div className="proposal-footer">
+            <p>Report applied</p>
+          </div>
+        </div>
+        <div className="proposal">
+          <div className="proposal-header">
+            <div className="proposal-header-img-freelancerInfo">
+              <div className="proposal-header-image">
+                <img src="https://via.placeholder.com/80x64" alt="freelancer photo" />
+              </div>
+              <div className="proposal-header-description">
+
+                <div className="name-tag">
+                  <div className="flag">
+                    <img src="https://via.placeholder.com/21x16" alt="flag of country name" />
+                  </div>
+                  <h2 className="freelancer">Mahmoud S.</h2>
+                  <p className="freelancer-name-tag">@mamoudBoraye88</p>
+                </div>
+                <div className="freelancer-rating">
+                  <RatingStars />
+                </div>
+
+
+              </div>
+            </div>
+            <div className="proposal-header-rate-time">
+              <h2 className="proposal-rate">$500.00 USD</h2>
+              <p className="proposal-estimation">in 7 days</p>
+            </div>
+          </div>
+          <div className="proposal-body">
+            <p>Hello, Hireprofl.
+              Reading job description carefully and noticed that you are going to develop job board with react and node js.
+              As MERN stack developer I am confident to complete this project and I can charge above requires.
+              I hope to work for you and I am sure that I will give you perfect result.
+              https://darkgalaxies.io/
+              https://www.gfinityesports.com/halo-infinite/halo-circle-store/
+              Thanks.
+              Best Regards.
+              Evegenia.</p>
+          </div>
+          <div className="proposal-footer">
+            <p>Report applied</p>
+          </div>
+        </div>
+        <div className="proposal">
+          <div className="proposal-header">
+            <div className="proposal-header-img-freelancerInfo">
+              <div className="proposal-header-image">
+                <img src="https://via.placeholder.com/80x64" alt="freelancer photo" />
+              </div>
+              <div className="proposal-header-description">
+
+                <div className="name-tag">
+                  <div className="flag">
+                    <img src="https://via.placeholder.com/21x16" alt="flag of country name" />
+                  </div>
+                  <h2 className="freelancer">Mahmoud S.</h2>
+                  <p className="freelancer-name-tag">@mamoudBoraye88</p>
+                </div>
+                <div className="freelancer-rating">
+                  <RatingStars />
+                </div>
+
+
+              </div>
+            </div>
+            <div className="proposal-header-rate-time">
+              <h2 className="proposal-rate">$500.00 USD</h2>
+              <p className="proposal-estimation">in 7 days</p>
+            </div>
+          </div>
+          <div className="proposal-body">
+            <p>Hello, Hireprofl.
+              Reading job description carefully and noticed that you are going to develop job board with react and node js.
+              As MERN stack developer I am confident to complete this project and I can charge above requires.
+              I hope to work for you and I am sure that I will give you perfect result.
+              https://darkgalaxies.io/
+              https://www.gfinityesports.com/halo-infinite/halo-circle-store/
+              Thanks.
+              Best Regards.
+              Evegenia.</p>
+          </div>
+          <div className="proposal-footer">
+            <p>Report applied</p>
+          </div>
+        </div>
       </section>
+
       <aside className="proposal-applied-AVG">
-        <div className="job-details-side-about-client"></div>
+        <div className="job-details-side-about-client">
+          <div className="bid">
+            <h3>Bids</h3>
+            <p>88</p>
+          </div>
+          <div className="line"></div>
+          <div className="avg-bid">
+            <h3>Avg Bids</h3>
+            <p>$593.00 USD</p>
+          </div>
+        </div>
       </aside>
     </section>
   );
