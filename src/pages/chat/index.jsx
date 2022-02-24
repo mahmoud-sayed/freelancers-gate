2d7d39e2b2ad002ca1f157da9f38063f80cf26cd
Mahmoud AlBoraey <mahmoudalsayied@gmail.com>
2022-02-24 20:55:11 +0200

    done making sidebar chat page chat

--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { BsSearch } from 'react-icons/bs';
 import './style.scss';
 
 const Chat = () => {
@@ -9,7 +9,67 @@ const Chat = () => {
                 <div className="content-wrapper">
 
                     <section className="clients-chats">
+                        <div className="clients-chat-wrapper">
+                            <div className="search-input">
+                                <BsSearch className="search-icon" />
+                                <input className="search" type="search" name="chat-search" id="" placeholder="search" />
+                            </div>
+                            <div className="chat-sidebar">
+                                <div className="client-chat-sidebar">
+                                    <div className="client-img"><img src="https://via.placeholder.com/50x50" alt="client imag" /></div>
+                                    <div className="client-name-and-lastSentMessage">
+                                        <div className="name-and-last-time-send">
+                                            <p className="client-name">Mahmoud S.</p>
+                                            <p className="last-time-send">10:34 AM</p>
+                                        </div>
+                                        <div className="lastSentMessage-and-notification-num">
+                                            <p className="last-sent-message">Hello world</p>
+                                            <p className="notification-num">1</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="client-chat-sidebar">
+                                    <div className="client-img"><img src="https://via.placeholder.com/50x50" alt="client imag" /></div>
+                                    <div className="client-name-and-lastSentMessage">
+                                        <div className="name-and-last-time-send">
+                                            <p className="client-name">Mahmoud S.</p>
+                                            <p className="last-time-send">10:34 AM</p>
+                                        </div>
+                                        <div className="lastSentMessage-and-notification-num">
+                                            <p className="last-sent-message">Hello world</p>
+                                            <p className="notification-num">12</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="client-chat-sidebar active">
+                                    <div className="client-img"><img src="https://via.placeholder.com/50x50" alt="client imag" /></div>
+                                    <div className="client-name-and-lastSentMessage">
+                                        <div className="name-and-last-time-send">
+                                            <p className="client-name">Mahmoud S.</p>
+                                            <p className="last-time-send">10:34 AM</p>
+                                        </div>
+                                        <div className="lastSentMessage-and-notification-num">
+                                            <p className="last-sent-message">Hello world</p>
+                                            <p className="notification-num">3</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="client-chat-sidebar">
+                                    <div className="client-img"><img src="https://via.placeholder.com/50x50" alt="client imag" /></div>
+                                    <div className="client-name-and-lastSentMessage">
+                                        <div className="name-and-last-time-send">
+                                            <p className="client-name">Mahmoud S.</p>
+                                            <p className="last-time-send">10:34 AM</p>
+                                        </div>
+                                        <div className="lastSentMessage-and-notification-num">
+                                            <p className="last-sent-message">Hello world</p>
+                                            <p className="notification-num">5</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
+                        </div>
                     </section>
                     <section className="chat-wrapper">
 
