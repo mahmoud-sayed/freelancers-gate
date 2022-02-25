ea3471e54d7cacdde19a202dde5da28110a64477
Mahmoud AlBoraey <mahmoudalsayied@gmail.com>
2022-02-25 02:39:06 +0200

    done making chat header and footer style

--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BsSearch } from 'react-icons/bs';
+import { BsSearch, BsPaperclip } from 'react-icons/bs';
+import { IoSendSharp } from 'react-icons/io5';
 import './style.scss';
 
 const Chat = () => {
@@ -10,10 +11,14 @@ const Chat = () => {
 
                     <section className="clients-chats">
                         <div className="clients-chat-wrapper">
-                            <div className="search-input">
-                                <BsSearch className="search-icon" />
-                                <input className="search" type="search" name="chat-search" id="" placeholder="search" />
-                            </div>
+                            <form className="search-form" action="">
+
+                                <div className="search-input">
+                                    <BsSearch className="search-icon" />
+                                    <input className="search" type="search" name="chat-search" id="" placeholder="search" />
+                                </div>
+
+                            </form>
                             <div className="chat-sidebar">
                                 <div className="client-chat-sidebar">
                                     <div className="client-img"><img src="https://via.placeholder.com/50x50" alt="client imag" /></div>
@@ -72,7 +77,32 @@ const Chat = () => {
                         </div>
                     </section>
                     <section className="chat-wrapper">
+                        <div className="chat-header">
+                            <div className="chat-client-img">
+                                <img src="https://via.placeholder.com/45x45" alt="client imag" />
+                            </div>
+                            <p className="chat-client-name">Mahmoud S.</p>
+                        </div>
+                        <div className="chat-body"></div>
+                        <div className="chat-footer-send-message">
+                            <form className="message-form" action="">
+                                <div class="file-upload">
+                                    <label htmlFor="file-input">
+                                        <BsPaperclip className='file-icon' />
+                                    </label>
+                                    <input id="file-input" type="file" />
+                                </div>
+                                <input type="text" placeholder="Type a message..." />
 
+
+                                <div className="send-message">
+                                    <label htmlFor="send-message-button">
+                                        <IoSendSharp className="send-message-icon" />
+                                    </label>
+                                    <button id="send-message-button"></button>
+                                </div>
+                            </form>
+                        </div>
                     </section>
                 </div>
             </div>
